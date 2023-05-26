71aea063d7fcb84439b039aa4c2b32b866b74682
Andrew Heim <andrewsheim@gmail.com>
2023-05-26 13:45:45 -0600

    h1 title change

--- src/app/page.js
+++ src/app/page.js
@@ -206,8 +206,8 @@ const userInputData = {
 export default async function Home() {
   return (
     <main>
-      <h1 className="header">
-        <span className="header-title">Portfolio Calculator</span>
+      <h1 className="flex justify-center mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
+        <span className="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Portfolio Calculator</span>
       </h1>
 
       <InputForm />
